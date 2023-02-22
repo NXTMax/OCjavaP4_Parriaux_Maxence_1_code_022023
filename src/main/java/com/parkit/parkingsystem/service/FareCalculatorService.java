138ad48f6d1ddd3c380c0f96012306797835dd6f
NXTMax <maxenceP25@gmail.com>
2023-02-22 23:27:38 +0100

    Use LocalDateTime to manage parking time spanning several days

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,28 +1,25 @@
 package com.parkit.parkingsystem.service;
 
+import java.time.Duration;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket) {
-        if((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+        if((ticket.getOutTime() == null) || (ticket.getOutTime().isBefore(ticket.getInTime()))) {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getMinutes();
-        int outHour = ticket.getOutTime().getMinutes();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        Duration ticketDuration = Duration.between(ticket.getInTime(), ticket.getOutTime());
 
         switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR / 60);
+                ticket.setPrice(ticketDuration.toMinutes() * Fare.CAR_RATE_PER_HOUR / 60);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR / 60);
+                ticket.setPrice(ticketDuration.toMinutes() * Fare.BIKE_RATE_PER_HOUR / 60);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
