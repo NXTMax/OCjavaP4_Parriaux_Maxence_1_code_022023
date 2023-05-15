843a82354a8233049a0f5e6c3233345be6ea2cef
NXTMax <maxenceP25@gmail.com>
2023-05-15 14:00:04 +0200

    Increase ParkingService code coverage

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -11,11 +11,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,16 +34,6 @@ public class ParkingServiceTest {
     @BeforeEach
     private void setUpPerTest() {
         try {
-            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
-            Ticket ticket = new Ticket();
-            ticket.setInTime(LocalDateTime.now().minusHours(1));
-            ticket.setParkingSpot(parkingSpot);
-            ticket.setVehicleRegNumber("ABCDEF");
-            when(ticketDAO.getTicket(anyString(), eq(getQueries.currentTicket))).thenReturn(ticket);
-            when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
-
-            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
-
             parkingService = new ParkingService(parkingSpotDAO, ticketDAO);
         } catch (Exception e) {
             e.printStackTrace();
@@ -48,10 +41,63 @@ public class ParkingServiceTest {
         }
     }
 
+    /**
+     * It should return a ticket bound to the given parking slot
+     */
     @Test
-    public void processExitingVehicleTest() {
-        parkingService.processExitingVehicle("ABCDEF");
-        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+    public void processIncomingVehicle_Test() {
+        when(ticketDAO.saveTicket(any(Ticket.class))).thenReturn(true);
+        when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+        when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
+
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+        assertEquals(parkingSpot, parkingService.processIncomingVehicle(ParkingType.CAR, "ABCDEF").getParkingSpot());
+    }
+
+    /**
+     * It should return null if there's no available parking slot or a database error
+     */
+    @Test
+    public void processIncomingVehicle_FailTest() {
+        when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(-1);
+        assertEquals(null, parkingService.processIncomingVehicle(ParkingType.CAR, "ABCDEF"));
+    }
+
+    /**
+     * It should fill in ticket's out time and price upon termination
+     */
+    @Test
+    public void processExitingVehicle_Test() {
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+        Ticket ticket = new Ticket();
+        ticket.setInTime(LocalDateTime.now().minusHours(1));
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEF");
+
+        when(ticketDAO.getTicket(anyString(), eq(getQueries.currentTicket))).thenReturn(ticket);
+        when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+        when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+        
+        Ticket terminatedTicket = parkingService.processExitingVehicle("ABCDEF");
+        assertNotNull(terminatedTicket.getOutTime());
+        assertNotEquals(0, terminatedTicket.getPrice());
+    }
+
+    /**
+     * It should return a null ticket if DAO fails to update the DB
+     */
+    @Test
+    public void processExitingVehicle_FailTest() {
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+        Ticket ticket = new Ticket();
+        ticket.setInTime(LocalDateTime.now().minusHours(1));
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEF");
+
+        when(ticketDAO.getTicket(anyString(), eq(getQueries.currentTicket))).thenReturn(ticket);
+        when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(false);
+        
+        assertNull(parkingService.processExitingVehicle("ABCDEF"));
     }
 
 }
